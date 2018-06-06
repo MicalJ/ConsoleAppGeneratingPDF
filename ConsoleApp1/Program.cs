f5ee478ad2f8606ffbb07e6ed3153e13de8a7342
Micał Jakub <jmical@opgksoftware.com>
2018-06-06 10:43:56 +0200

    Generate PDF for linux

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp1.Services;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Setup DI");
+            var serviceProvider = new ServiceCollection()
+            .AddSingleton<IProtocolService, ProtocolService>()
+            .AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()))
+            .BuildServiceProvider();
+
+            Console.WriteLine("Do the actual work...");
+            var protocolService = serviceProvider.GetService<IProtocolService>();
+
+            protocolService.GeneratePDF();
+
+            Console.WriteLine("All done!");
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ ConsoleApp1/Services/ProtocolService.cs
@@ -0,0 +1,58 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using System;
+using System.IO;
+
+namespace ConsoleApp1.Services
+{
+    public class ProtocolService : IProtocolService
+    {
+        private readonly IConverter _converter;
+
+        public ProtocolService(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+        public void GeneratePDF()
+        {
+           var converter = new BasicConverter(new PdfTools());
+
+            string directory = "Protocols";
+            if (!Directory.Exists(Path.GetFullPath(directory)))
+            {
+                Directory.CreateDirectory(Path.GetFullPath(directory));
+            }
+
+            string fileName = "protocol_" + Guid.NewGuid() + ".pdf";
+            string protocolFile = Path.Combine(directory, fileName);
+
+            var doc = new HtmlToPdfDocument()
+            {
+                GlobalSettings =
+                {
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4,
+                    Margins = new MarginSettings
+                    {
+                        Top = 10,
+                        Bottom = 10
+                    },
+                    Out = protocolFile,
+                },
+                Objects =
+                {
+                    new ObjectSettings
+                    {
+                        HtmlContent = "<!doctype html> <html><body>TEST</body></html>",
+                        PagesCount=true,
+                        FooterSettings = {Right="strona [page] stron [toPage]", Line=true, FontSize=7},
+                        WebSettings = {DefaultEncoding="utf-8",EnableIntelligentShrinking = true}
+                    }
+                }
+            };
+
+            converter.Convert(doc);
+        }
+    }
+}
